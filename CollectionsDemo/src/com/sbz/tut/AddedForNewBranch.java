de0e572b07c6c1fd4a4780dd71ba16ed772d2710
shahbazma <shahbazma@SHAHBAZ-PC>
2019-11-17 16:43:45 +0530

    Testing mulii line

--- CollectionsDemo/src/com/sbz/tut/AddedForNewBranch.java
+++ CollectionsDemo/src/com/sbz/tut/AddedForNewBranch.java
@@ -3,8 +3,9 @@ package com.sbz.tut;
 public class AddedForNewBranch {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hello World");
+		int a=0;
+		System.out.println("Hello  is chnaged");
+		System.out.println("Hello World is chnaged");
 		
 		//smaskmakls
 	}
